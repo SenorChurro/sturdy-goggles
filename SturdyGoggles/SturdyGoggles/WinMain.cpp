e97a201d39f4b689ea84e4498459895b94dea020
SenorChurro <parker.brian.dev@gmail.com>
2020-03-29 16:05:43 -0700

    Setting up the WinMain project
    set the setings for the project and also the initial window class

---
+++ SturdyGoggles/SturdyGoggles/WinMain.cpp
@@ -0,0 +1,41 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	// create and register the window
+	const auto pClassName = "SturdyGoggles";
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx(&wc);
+
+	// create window
+	HWND hWnd = CreateWindowExA(
+		0,
+		pClassName,
+		"SturdyGoggles Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+		);
+
+	//Show the window
+	ShowWindow(hWnd, SW_SHOW);
+
+	return 0;
+}
