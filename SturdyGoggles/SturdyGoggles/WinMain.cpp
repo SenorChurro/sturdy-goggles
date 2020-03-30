032734bfa13dcbd24266778bfb0b064455abda44
SenorChurro <parker.brian.dev@gmail.com>
2020-03-29 21:15:26 -0700

    Setting up the foundation for rendering a window

--- SturdyGoggles/SturdyGoggles/WinMain.cpp
+++ SturdyGoggles/SturdyGoggles/WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT	CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -11,7 +22,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -34,8 +45,25 @@ int CALLBACK WinMain(
 		nullptr, nullptr, hInstance, nullptr
 		);
 
-	//Show the window
+	// show the window
 	ShowWindow(hWnd, SW_SHOW);
+
+	// get the messages for the window
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 
 	return 0;
 }
