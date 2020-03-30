90d80785672ef37b2f18bfb76799c7ed762e29db
SenorChurro <parker.brian.dev@gmail.com>
2020-03-29 23:06:07 -0700

    including exception handling for the windows class

--- SturdyGoggles/SturdyGoggles/Window.h
+++ SturdyGoggles/SturdyGoggles/Window.h
@@ -34,7 +34,7 @@ private:
 		HINSTANCE hInst;
 	};
 public:
-	Window(int width, int height, const char* name) noexcept;
+	Window(int width, int height, const char* name);
 	~Window();
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
@@ -49,5 +49,5 @@ private:
 };
 
 // error exception helper macro
-#define CHWND_EXCEPT( hr ) Window::Exception( __LINE__,__FILE__,hr )
-#define CHWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
+#define SGWND_EXCEPT( hr ) Window::Exception( __LINE__,__FILE__,hr )
+#define SGWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
