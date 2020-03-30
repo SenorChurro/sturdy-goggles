ce8811529cee9c6484e00d71a1283f9a4a66ca2d
SenorChurro <parker.brian.dev@gmail.com>
2020-03-29 22:18:04 -0700

    Creating the window class

--- SturdyGoggles/SturdyGoggles/WinMain.cpp
+++ SturdyGoggles/SturdyGoggles/WinMain.cpp
@@ -1,15 +1,4 @@
-#include <Windows.h>
-
-LRESULT	CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(0);
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "Window.h"
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -17,36 +6,7 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
-	// create and register the window
-	const auto pClassName = "SturdyGoggles";
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-
-	RegisterClassEx(&wc);
-
-	// create window
-	HWND hWnd = CreateWindowExA(
-		0,
-		pClassName,
-		"SturdyGoggles Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-		);
-
-	// show the window
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 300, "Sturdy Goggles");
 
 	// get the messages for the window
 	MSG msg;
@@ -56,14 +16,11 @@ int CALLBACK WinMain(
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	if (gResult == -1)
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
 
-	return 0;
+	return msg.wParam;
 }
---
+++ SturdyGoggles/SturdyGoggles/WindowsMessageMap.h
@@ -0,0 +1,16 @@
+/******************************************************************************************
+	Provided by ChiliTomatoNoodle
+	Boiler Plate for handling windows messages
+******************************************************************************************/
+#pragma once
+#include <unordered_map>
+#include <Windows.h>
+
+class WindowsMessageMap
+{
+public:
+	WindowsMessageMap() noexcept;
+	std::string operator()(DWORD msg, LPARAM lp, WPARAM wp) const noexcept;
+private:
+	std::unordered_map<DWORD, std::string> map;
+};
