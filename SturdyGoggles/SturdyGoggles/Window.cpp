89452957e617b0337748b2ec02f346eee6e64903
SenorChurro <parker.brian.dev@gmail.com>
2020-04-05 21:24:19 -0700

    Implementing some debug tools and diagnostics

--- SturdyGoggles/SturdyGoggles/Window.cpp
+++ SturdyGoggles/SturdyGoggles/Window.cpp
@@ -122,6 +122,10 @@ std::optional<int> Window::ProcessMessages()
 
 Graphics& Window::Gfx()
 {
+	if (!pGfx)
+	{
+		throw SGWND_NOGFX_EXCEPT();
+	}
 	return *pGfx;
 }
 
@@ -254,27 +258,6 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
-	:
-	SGException(line, file),
-	hr(hr)
-{}
-
-const char* Window::Exception::what() const noexcept
-{
-	std::ostringstream oss;
-	oss << GetType() << std::endl
-		<< "[Error Code] " << GetErrorCode() << std::endl
-		<< "[Description] " << GetErrorString() << std::endl
-		<< GetOriginString();
-	whatBuffer = oss.str();
-	return whatBuffer.c_str();
-}
-
-const char* Window::Exception::GetType() const noexcept
-{
-	return "Sturdy Goggles Window Exception";
-}
 
 std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 {
@@ -298,12 +281,42 @@ std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 	return errorString;
 }
 
-HRESULT Window::Exception::GetErrorCode() const noexcept
+HRESULT Window::HrException::GetErrorCode() const noexcept
 {
 	return hr;
 }
 
-std::string Window::Exception::GetErrorString() const noexcept
+Window::HrException::HrException(int line, const char* file, HRESULT hr) noexcept
+	:
+	Exception(line, file),
+	hr(hr)
+{}
+
+const char* Window::HrException::what() const noexcept
 {
-	return TranslateErrorCode(hr);
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< "[Description] " << GetErrorDescription() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Window::HrException::GetType() const noexcept
+{
+	return "Sturdy Goggles Window Exception";
+}
+
+
+std::string Window::HrException::GetErrorDescription() const noexcept
+{
+	return Exception::TranslateErrorCode(hr);
+}
+
+
+const char* Window::NoGfxException::GetType() const noexcept
+{
+	return "Chili Window Exception [No Graphics]";
 }
--- SturdyGoggles/SturdyGoggles/Window.h
+++ SturdyGoggles/SturdyGoggles/Window.h
@@ -6,20 +6,33 @@
 #include "Graphics.h"
 #include <memory>
 #include <optional>
+
+
 class Window
 {
 public:
 	class Exception : public SGException
 	{
+		using SGException::SGException;
 	public:
-		Exception(int line, const char* file, HRESULT hr) noexcept;
-		const char* what() const noexcept override;
-		virtual const char* GetType() const noexcept;
 		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
-		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
 	private:
 		HRESULT hr;
+	};
+	class NoGfxException : public Exception
+	{
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
 	};
 private:
 	// singleton manages registration/cleanup of window class
@@ -61,5 +74,6 @@ private:
 };
 
 // error exception helper macro
-#define SGWND_EXCEPT( hr ) Window::Exception( __LINE__,__FILE__,hr )
-#define SGWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
+#define SGWND_EXCEPT( hr ) Window::HrException( __LINE__,__FILE__,(hr) )
+#define SGWND_LAST_EXCEPT() Window::HrException( __LINE__,__FILE__,GetLastError() )
+#define SGWND_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
